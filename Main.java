606e682597ea246c0f6709b445179ad62ee0a55e
suge kanu <suge.kanu@gmail.com>
2022-12-05 23:40:34 +0000

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Enter your name");
+
+    Scanner in = new Scanner(System.in);
+    String s = in.nextLine();
+
+    if(s.equals("Kanu")){
+      System.out.println("Hey Kanu!");
+    }else if(s.equals("Suge")){
+      System.out.println("Hey Suge!");
+      
+    }else{
+      System.out.println("You are not welcome here");
+    }
+
+    in.close();
   }
 }
